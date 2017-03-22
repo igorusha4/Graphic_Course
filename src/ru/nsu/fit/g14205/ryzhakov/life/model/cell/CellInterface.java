40ff6cf892580934a2043fac326bea5669f2c41c
igorusha4 <igorusha4@yandex.ru>
2017-03-22 14:21:53 +0700

    Удалил для пул реквеста

--- src/ru/nsu/fit/g14205/ryzhakov/life/model/cell/CellInterface.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.fit.g14205.ryzhakov.life.model.cell;
-
-public interface CellInterface {
-    int getWidth();
-    int getHeight();
-    char getCellState(int x, int y);
-    double getCellValue(int x, int y);
-}
--- src/ru/nsu/fit/g14205/ryzhakov/life/view/settingsWindow/ChangeListener.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.fit.g14205.ryzhakov.life.view.settingsWindow;
-
-import ru.nsu.fit.g14205.ryzhakov.life.CellSettings;
-import ru.nsu.fit.g14205.ryzhakov.life.model.GameSettings;
-
-public interface ChangeListener {
-    void run(CellSettings cellSettings, int width, int height, GameSettings gameSettings, boolean xorMode);
-}
